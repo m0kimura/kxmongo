49f901a54e9f6142a6985e8a1ac4d6d921debff9
Michihiro Kimura <kimura@kmrweb.net>
2016-10-26 23:34:13 +0900

    first confirm

--- kxmongo.js
+++ kxmongo.js
@@ -1,5 +1,5 @@
 //#######1#########2#########3#########4#########5#########6#########7#########8#########9#########0
-var Mongo=require('knuty');
+var Mongo=require('kxuty');
 var Mg=require('mongodb');
 
 Mongo.extend({
